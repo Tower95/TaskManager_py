70ffa14c223f7399a0c0d4f0efd28dd76142253b
Ismael Torres <torrestmi3@gmail.com>
2026-01-19 23:41:24 -0700

    Working v1.0

---
+++ database.py
@@ -0,0 +1,73 @@
+import os
+import mysql.connector
+from mysql.connector import Error
+from dotenv import load_dotenv
+
+# load file wiht .env just start the scirpt
+load_dotenv();
+
+class TaskManagerDB:
+    def __init__(self):
+        self.host = os.getenv('DB_HOST')
+        self.user = os.getenv('DB_USER')
+        self.password = os.getenv('DB_PASS')
+        self.database = os.getenv('DB_NAME')
+
+    def _get_connection(self):
+        """
+        PRIVATE METHOD (start with _ )
+        only use inner this calss
+        """
+
+        try:
+            conn = mysql.connector.connect(
+                    host=self.host,
+                    user=self.user,
+                    password=self.password,
+                    database=self.database,
+                    charset='utf8mb4'
+                    )
+            return conn
+        except Error as e:
+            print(f"❌ Error connecting to MySQL:{e}")
+            return None
+
+    def insert_task(self, title, description, base64_data=None):
+
+        connection = self._get_connection()
+
+        if connection:
+            cursor = connection.cursor()
+            query = """INSERT INTO tasks (title,description,data_base64)
+                       VALUES (%s,%s,%s)"""
+
+            cursor.execute(query, (title,description,base64_data))
+            connection.commit()
+            cursor.close()
+            connection.close()
+            print("✅ Tarea guardada con éxito.")
+
+    def list_task(self):
+        connection = self._get_connection()
+
+        if connection:
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM tasks WHERE is_deleted = 0 ORDER BY created_at DESC")
+            result = cursor.fetchall()
+            connection.close()
+            return result
+        return []
+
+    def soft_delete(self, task_id):
+        connection = self._get_connection()
+        if connection:
+            cursor = connection.cursor()
+            query = "UPDATE tasks SET is_deleted = 1 WHERE id = %s"
+            cursor.execute(query, (task_id,))
+            connection.commit()
+            cursor.close()
+            connection.close()
+
+
+
+
--- main.py
+++ main.py
@@ -1,8 +1,48 @@
-import mysql.connector
+import os
+from database import TaskManagerDB 
 
-# Define the connection parameters
-config = {
-        'user':'root'
-        'password':'example',
-        'host': 'localhost',
-        'database':
+def clean_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def menu():
+    print("\n--- TASK MANAGER v1.0 (CLI) ---")
+    print("1. Ver tareas")
+    print("2. Agregar tarea")
+    print("3. Borrar tarea")
+    print("4. Salir")
+
+def excecute():
+
+    db = TaskManagerDB()
+    while True:
+        menu()
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            clean_screen()
+            tasks = db.list_task()
+            if not tasks:
+                print("No hay tareas pendientes. ¡Relájate! 😎")
+            for t in tasks: print(f"[{t['id']}] [[{t['title']}] - [{t['status']}] ({t['created_at']})") 
+
+        elif opcion == "2":
+            title = input("Titulo: ")
+            desc = input("Descripción: ")
+            db.insert_task(title,desc)
+            print("¡Tarea anotada!")
+
+        elif opcion =="3":
+            task_id = input("ID de la tarea a borrar: ")
+            db.soft_delete(task_id)
+            print(f"Tarea {task_id} enviada a la papaelera.")
+
+        elif opcion == "4":
+            print("¡Adiós! que tengas un día productivo .")
+            break
+
+        else:
+            print("Opción no válida, intenta de nueveo.")
+
+if __name__ == "__main__":
+    excecute()
+
